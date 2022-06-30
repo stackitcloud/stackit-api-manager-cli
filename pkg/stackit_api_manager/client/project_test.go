b7c3f7be2eab296c3f13f6a61550c6e8cebf4709
Steffen Exler <Steffen.Exler@mail.schwarz>
2022-06-30 15:34:52 +0200

    Add test for ProjectRetire
    Signed-off-by: Steffen Exler <Steffen.Exler@mail.schwarz>

--- pkg/stackit_api_manager/client/project_test.go
+++ pkg/stackit_api_manager/client/project_test.go
@@ -18,15 +18,15 @@ func TestEncodeBase64File(t *testing.T) {
 		{
 			name: "success",
 			args: args{
-				file: "./testdata/test.yaml",
+				file: "./testdata/test.json",
 			},
-			want:    "aGVsbG86IGR1ZGV0dGUK",
+			want:    "eyAiaGVsbG8iOiAiZHVkZXR0ZSIgfQo=",
 			wantErr: false,
 		},
 		{
 			name: "file not found",
 			args: args{
-				file: "./testdata/test-not-found.yaml",
+				file: "./testdata/test-not-found.json",
 			},
 			wantErr: true,
 		},
@@ -87,15 +87,21 @@ func TestClient_ProjectPublish(t *testing.T) {
 				Message: stringPtn("Success"),
 			},
 		},
+		{
+			name: "invalid http response",
+			args: args{
+				projectID:      "some-project-id",
+				projectPublish: &ProjectPublish{},
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := mockClient(t)
-
 			for _, mockResponse := range tt.mockResponses {
 				mockJSONHTTPResponse(t, mockResponse.path, mockResponse.body)
 			}
-
 			got, _, err := c.ProjectPublish(tt.args.projectID, tt.args.projectPublish)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Client.ProjectPublish() error = %v, wantErr %v", err, tt.wantErr)
@@ -103,6 +109,70 @@ func TestClient_ProjectPublish(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Client.ProjectPublish() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestClient_ProjectRetire(t *testing.T) {
+	type args struct {
+		projectID     string
+		projectRetire *ProjectRetire
+	}
+	tests := []struct {
+		name          string
+		args          args
+		mockResponses []mockResponses
+		want          *ProjectRetireResponse
+		wantErr       bool
+	}{
+		{
+			name: "success",
+			args: args{
+				projectID: "some-project-id",
+				projectRetire: &ProjectRetire{
+					Metadata: &Metadata{
+						Identifier: stringPtn("some-identifier"),
+						Stage:      stringPtn("some-stage"),
+					},
+				},
+			},
+			mockResponses: []mockResponses{
+				{
+					path: "/v1/projects/some-project-id/retire",
+					body: ProjectPublishResponse{
+						Code:    intPtn(200),
+						Message: stringPtn("Success"),
+					},
+				},
+			},
+			want: &ProjectRetireResponse{
+				Code:    intPtn(200),
+				Message: stringPtn("Success"),
+			},
+		},
+		{
+			name: "invalid http response",
+			args: args{
+				projectID:     "some-project-id",
+				projectRetire: &ProjectRetire{},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := mockClient(t)
+			for _, mockResponse := range tt.mockResponses {
+				mockJSONHTTPResponse(t, mockResponse.path, mockResponse.body)
+			}
+			got, _, err := c.ProjectRetire(tt.args.projectID, tt.args.projectRetire)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Client.ProjectRetire() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Client.ProjectRetire() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
