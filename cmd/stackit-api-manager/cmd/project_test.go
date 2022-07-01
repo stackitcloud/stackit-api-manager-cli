2fab3fb9d61b1bb2f2dd71ecc30ed482eebe107d
Steffen Exler <Steffen.Exler@mail.schwarz>
2022-07-01 11:14:56 +0200

    add test
    Signed-off-by: Steffen Exler <Steffen.Exler@mail.schwarz>

--- cmd/stackit-api-manager/cmd/project_test.go
+++ cmd/stackit-api-manager/cmd/project_test.go
@@ -25,3 +25,43 @@ func Test_newAPIClient(t *testing.T) {
 		})
 	}
 }
+
+func Test_newMetadata(t *testing.T) {
+	testIdentifier := "identifier-test"
+	testStage := "stage-test"
+	emptyString := ""
+
+	tests := []struct {
+		name       string
+		want       *client.Metadata
+		identifier string
+		stage      string
+	}{
+		{
+			name: "success with empty values",
+			want: &client.Metadata{
+				Identifier: &emptyString,
+				Stage:      &emptyString,
+			},
+		},
+		{
+			name: "success with test values",
+			want: &client.Metadata{
+				Identifier: &testIdentifier,
+				Stage:      &testStage,
+			},
+			identifier: "identifier-test",
+			stage:      "stage-test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			identifier = tt.identifier
+			stage = tt.stage
+
+			if got := newMetadata(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newMetadata() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
